58a7eeca15c3781f2ac4d3d0e3f25374b74bf4f4
kmc97 <kmc97@duke.edu>
2018-02-08 12:43:54 -0500

    finds max/min and returns tuple

---
+++ return_max_min.py
@@ -0,0 +1,17 @@
+
+def find_min(number_list):
+    import numpy as np
+    min_ = np.min(number_list)
+    return min_
+
+def find_max(number_list):
+    import numpy as np
+    max_ = np.max(number_list)
+    return max_
+
+def tuple_generator(min_, max_):
+    import numpy as np
+    min_max = (min_, max_)
+    return min_max
+
+
