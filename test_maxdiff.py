9f17cdd45c12c5584abcd9821fecab2f89f0a0ae
Souhaila Noor <souhaila.noor@duke.edu>
2018-02-10 14:42:19 -0500

    Returns max difference between two adjacent numbers

--- test_maxdiff.py
+++ test_maxdiff.py
@@ -1,5 +1,6 @@
-def test_max_diff(input):
-	from CoolKids_testing import maxdiff
-	maxdiff = input([8,10,9,6])
-	assert maxdiff == 3
+def test_max_diff():
+	from maxdiff import maxdiff
+	maxdiff_output = maxdiff([1,2,4,7])
+	assert maxdiff_output == 3
+	
 	
