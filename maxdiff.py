71f3da9b1a991e2b7f03dda5df785f204a009400
Souhaila Noor <souhaila.noor@duke.edu>
2018-02-10 14:03:24 -0500

    returns max difference

--- maxdiff.py
+++ maxdiff.py
@@ -1,3 +1,9 @@
 def maxdiff(input):
-	
+	"""returns the max difference between two adjacent numbers"""
+	import numpy as np
+	input = [8,10,9,6]
+	diff = np.diff(input)
+	maxdiff = diff.max()
+	return maxdiff
+		
 	
---
+++ test_maxdiff.py
@@ -0,0 +1,5 @@
+def test_max_diff(input):
+	from CoolKids_testing import maxdiff
+	maxdiff = input([8,10,9,6])
+	assert maxdiff == 3
+	
