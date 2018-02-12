7d1db18c9ea1fa83ad53b199f436f00c21613255
kmc97 <kmc97@duke.edu>
2018-02-12 13:09:09 -0500

    type error and logging correct

--- return_max_min.py
+++ return_max_min.py
@@ -1,14 +1,38 @@
-def find_min(number_list):
-    import numpy as np
-    min_ = np.min(number_list)
-    return min_
 
-def find_max(number_list):
-    import numpy as np
-    max_ = np.max(number_list)
-    return max_
+import numpy as np
+import logging
 
-def tuple_generator(min_, max_):
-    min_max = (min_, max_)
-    return min_max
+logging.basicConfig(filename = 'logging.txt', format= '%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level =logging.DEBUG)
+
+def find_min_max(number_list):
+    """fxn takes list of integers and returns min and max in a tuple
+
+    :param number_list: list of integers
+    :raises TypeError: type error if string in list
+    :returns max_min: a tuple containing the max and min of a list
+
+    """
+ 
+    try:
+        min_ =np.min([number_list])
+        max_ =np.max([number_list])
+    except TypeError:
+         logging.debug('DEBUG: Try checking the data type of number_list')
+         logging.warning('WARNING: Your Program may blow up from the wrong data type')   
+         raise TypeError #("you have the wrong Data type")
+ 
+    else: 
+        min_ = np.min(number_list)
+        max_ = np.max(number_list)
+        max_min = (min_, max_)
+   
+    return max_min
+
+
+def main(number_list):
+    logging.info('INFO: Program Started')
+    max_min = find_min_max(number_list)
+    logging.info('INFO: Program Ended')
+
+    return max_min
 
--- test_return_max_min.py
+++ test_return_max_min.py
@@ -1,27 +1,29 @@
 # testing return max and min function
+import pytest
 
 def test_min():
-    from return_max_min import find_min
-    min_ = find_min([4, 7,-20])
-    assert min_ == -20
+    from return_max_min import find_min_max
+    min_ = find_min_max([1312, -20, 12312])
+    assert min_[0] == -20
 
 def test_max():
-    from return_max_min import find_max
-    max_ = find_max([1010, 32 -302931])
-    assert max_ == 1010
-
-def test_doubles():
-    from return_max_min import find_max
-    max_ = find_max([2, 2, 0])
-    assert max_ == 2
+    from return_max_min import find_min_max
+    max_ = find_min_max([1010, 32 -302931])
+    assert max_[1] == 1010
 
 def test_precision():
-    from return_max_min import find_max
-    max_ = find_max([1.33, 0, -222])
-    assert max_ == 1.33
+    from return_max_min import find_min_max
+    value = find_min_max([1.33, 0, -222])
+    assert value == (-222, 1.33)
 
 def test_tuple_size():
-    from return_max_min import tuple_generator
-    min_max = tuple_generator(23, 53)
+    from return_max_min import find_min_max
+    min_max = find_min_max([23, 53, 234234, 12, -3929423])
     assert len(min_max) == 2
+
+def test_try_exception_type():
+    from return_max_min import find_min_max
+    with pytest.raises(TypeError):
+        find_min_max(['a',7,-20])
+  
 
