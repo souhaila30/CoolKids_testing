24f3cd902ca9a231c770d1b62b22c2ba445ba55f
kmc97 <kmc97@duke.edu>
2018-02-14 12:56:11 -0500

    corrected test document to match updated py file

--- test_all_functions.py
+++ test_all_functions.py
@@ -9,7 +9,7 @@ list_of_numbers_3 = [-7, -8, 26, 54, -2]
 
 def test_max_diff():
     from maxdiff import maxdiff
-    maxdiff_output = maxdiff([1,2,4,7)
+    maxdiff_output = maxdiff([1,2,4,7])
     assert maxdiff_output == 3
 
 def test_sum_list_integer():
@@ -31,24 +31,24 @@ def test_sum_list_negative():
 
 
 def test_min():
-    from return_max_min import find_min
-    min_ = find_min([4, 7,-20])
-    assert min_ == -20
+    from return_max_min import find_min_max
+    min_ = find_min_max([4, 7,-20])
+    assert min_[0] == -20
 
 
 def test_max():
-    from return_max_min import find_max
-    max_ = find_max([1010, 32 -302931])
-    assert max_ == 1010
+    from return_max_min import find_min_max
+    max_ = find_min_max([1010, 32 -302931])
+    assert max_[1] == 1010
 
 
 def test_precision():
-    from return_max_min import find_max
-    max_ = find_max([1.33, 0, -222])
-    assert max_ == 1.33
+    from return_max_min import find_min_max
+    max_ = find_min_max([1.33, 0, -222])
+    assert max_ == (-222, 1.33)
 
 
 def test_tuple_size():
-    from return_max_min import tuple_generator
-    min_max = tuple_genierator(23, 53)
+    from return_max_min import find_min_max
+    min_max = find_min_max([23, 53,123123123, -13934])
     assert len(min_max) == 2
