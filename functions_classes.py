ce86c5ffeefb2f9f3fb3bde0546bfa679411a699
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-02-22 23:13:33 -0500

    Updated files - object oriented programming

---
+++ functions_classes.py
@@ -0,0 +1,114 @@
+class Functions:
+    num_list = []
+
+    """
+
+    Functions class has attributes of sum, a max and min tuple, and max difference
+
+    """
+
+    def __init__(self, num_list):
+
+        """
+
+        Initialized the attributes of the class Functions
+
+        """
+
+        self.num_list = num_list
+
+    import logging
+    logging.basicConfig(filename = 'logging.txt', format = '%(asctime)s %(message)s', datefmt = '%m/%d/%Y &I:%M:%S %p', level = logging.DEBUG)
+    def find_sum(num_list):
+
+        """
+
+        sum functions takes a list of numbers and returns a sum
+
+        :param num_list: list of numbers
+        :returns list_sum :sum of list of numbers
+
+        """
+        import numpy as np
+        list_sum = np.sum(num_list)
+
+        return  list_sum
+
+    def find_max_diff(num_list):
+
+        """
+
+        function takes a list of integers and returns the max difference between two adjacent
+        numbers
+
+        :param num_list: list of numbers
+        :returns max_diff: maximum difference
+
+        """
+
+        import numpy as np
+        diff = np.diff(num_list)
+        maxdiff = diff.max()
+
+        return maxdiff
+
+    def find_min_max(num_list):
+
+        """
+
+        Function takes a list of numbers and returns min and max in a tuple
+
+        :param num_list: list of numbers
+        :returns min_max: a tuple containing the max and the min
+
+        """
+
+        min_list = np.min(num_list)
+        max_list = np.max(num_list)
+        min_max = (min_list, max_list)
+
+        return min_max
+
+    def find_exceptions(num_list):
+
+        """
+
+        fucntion checks and raises appropriate excpetions: type, value, import
+
+        :param num_list: list of numbers
+        :raises TypeError: checks to make sure only integers are in the list of numbers
+        :raises ImportError: checks to make sure the correct files are imported
+        :raises ValueError: make sure the integers have real values
+
+        """
+
+        try: 
+            find_sum(num_list)
+            find_max_diff(num_list)
+            find_min_max(num_list)
+
+        except TypeError:
+            logging.debug('DEBUG: Check the data type in the list of numbers')
+            logging.warning('WARNING: You have the wrong data type, be careful')
+            raise TypeError
+
+        except ImportError:
+            logging.debug('DEBUG:Error in the imported file')
+            logging.warning('WARNING: File DNE')
+            raise ImportError
+
+        except ValueError:
+            logging.debug('DEBUG: value error, check the list of numbers')
+            logging.warning('WARNING: value error, value other than an integer is an input')
+            raise ValueError
+
+    def main(num_list):
+       logging.info('INFO: Program started')
+       find_exceptions(num_list)
+       sum_  = find_sum(num_list)
+       max_min = find_min_max(num_list)
+       max_diff = find_max_diff(num_list)
+       logging.info('INFO:Program ended')
+
+       return sum_, max_min, max_diff
+
--- test_all_functions.py
+++ test_all_functions.py
@@ -8,63 +8,63 @@ list_of_numbers_3 = [-7, -8, 26, 54, -2]
 
 
 def test_max_diff():
-    from all_functions import maxdiff
-    maxdiff_output = maxdiff([1,2,4,7])
+    from functions_classes import find_max_diff
+    maxdiff_outputf = maxdiff([1,2,4,7])
     assert maxdiff_output == 3
 
 
 def test_sum_list_integer():
-    from all_functions import sum_list
+    from function_classes import find_sum
     sum_ = sum_list(list_of_numbers_1)
     assert sum_ == 164
 
 
 def test_sum_list_decimals():
-    from all_functions import sum_list
+    from functions_classes import find_sum
     sum_ = sum_list(list_of_numbers_2)
     assert sum_ == 51.21
 
 
 def test_sum_list_negative():
-    from all_functions import sum_list
+    from functions_classes import find_sum
     sum_ = sum_list(list_of_numbers_3)
     assert sum_ == 63
 
 
 def test_min():
-    from all_functions import find_min_max
+    from functions_classes import find_min_max
     min_ = find_min_max([4, 7,-20])
     assert min_[0] == -20
 
 
 def test_max():
-    from all_functions import find_min_max
+    from functions_classes import find_min_max
     max_ = find_min_max([1010, 32 -302931])
     assert max_[1] == 1010
 
 
 def test_precision():
-    from all_functions import find_min_max
+    from functions_classes import find_min_max
     max_ = find_min_max([1.33, 0, -222])
     assert max_ == (-222, 1.33)
 
 
 def test_tuple_size():
-    from all_functions import find_min_max
+    from functions_classes import find_min_max
     min_max = find_min_max([23, 53,123123123, -13934])
     assert len(min_max) == 2
 
 def test_try_exception_type():
-    from all_functions import find_exceptions
+    from functions_classes import find_exceptions
     with pytest.raises(TypeError):
         find_exceptions(['a', 7, -20])
 
 def test_try_exception_import():
-    from all_functions import find_exceptions
+    from functions_classes import find_exceptions
     with pytest.raises(ImportError):
         import ThisIsNotAFile
 
 def test_try_exception_value():
-    from all_functions import find_exceptions
+    from functions_classes import find_exceptions
     with pytest.raises(ValueError):
         find_exceptions([math.sqrt(-12421412), 23232, -12412])
